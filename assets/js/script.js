52f38c548d2e7135a70cab034311cdc062b6d34c
unknown <90293855+fishdestroyer@users.noreply.github.com>
2021-11-08 18:49:16 -0600

    update with additional questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,30 @@ var quizTimer;
 var feedBackEl = document.querySelector("#feedback");
 const questions = [
     {
-        question:"In what time frame was coding languages invented/ developed? ";
+        question:"In what time frame was modern coding languages invented/ developed? ",
         choices:["'The 1940's", "The 1950's", "The 1960's", "The 1970's"],
         answer: "The 1950's" 
     },
 
-    {
+    {// wikipedia
         question:"What is the first commercially available programming language?",
         choices:["Fortran", "Python", "C++", "Javascript"],
         answer:"Fortran" 
     },
 
-    {
+    {//Wikipedia
         question:"What is recognized as the first published computer program?",
         choices:["Short code", "Fortran", "Plankalkul", "Algorithm for the Analytical Engine"],
         answer:"Algorithm for the Analytical Engine"
 
+    },
+
+    {
+        question:"What language did Alick Glennie develop?",
+        choices:["Fortran", "Autocode", "FLOW-MATIC", "Comtran"],
+        answe:"Autocode"
+
     }
 
-    
+
 ]
