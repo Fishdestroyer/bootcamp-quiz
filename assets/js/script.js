68551d83f40129950937064bd81d20acdd3aa1a5
Fishdestroyer <jeffwhitner@gmail.com>
2022-01-11 09:34:03 -0600

    Update scripts.js with quiz coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,11 +103,105 @@ function endQuiz() {
     var finalQuizDetails = `
     <h2>Great job!</h2>
     <h3>Your final score is ` + totalScore + `!</h3>
-    <h3>Please enter your initials to submit your score in the highscore chart.</h3>
-    <input type="text" id="name" placeholder="Please enter your initals>
+    <h3>Please enter your initials below.</h3>
+    <input type="text" id="name" placeholder="Please enter your initals">
     <button onclick="saveHighscore()" class="highscore-btn" title="Submit Highscore">Submit</button>`;
 
     document.getElementById("startpage").innerHTML = finalQuizDetails;
 }
 //demonstrates player highscore saved in their own localstorage
+function saveHighscore() {
+    localStorage.setItem("saveHighscore", totalScore);
+    localStorage.setItem("Name", document.getElementById('name').value);
 
+    getHighscore();
+}
+
+//after player saves highscore, turns into special page that allows user to either clear score and play again or simply play again without affecting their score on localstorage
+
+function getHighscore() {
+    var finalQuizDetails = `
+    <h2>` + localStorage.getItem("Name") + `'s highscore is:</h2>
+    <h1>` + localStorage.getItem("saveHighscore") + `</h1><br >
+    <button onclick="clearHighscore()" class="clear-btn" title = "Clear Score and Play Again!">Clear Score and Play Again!</button>`;
+        document.getElementById("startpage").innerHTML = finalQuizDetails;
+}
+// clear function clears high score from the user's local storage and brings the user back to the start page
+function clearHighscore() {
+    localStorage.setItem("Highscore", "");
+    localStorage.setItem("name", "");
+
+    resetGame();
+}
+
+//reset function puts user back to the beginning page and allows them to play the quiz again!
+function resetGame() {
+    clearInterval(quizTimer);
+    totalScore = 0;
+    nextQuestion = -1;
+    timeLeft = 0;
+    quizTimer = null;
+
+    document.getElementById("timeLeft").innerHTML = timeLeft + " seconds";
+
+    window.location.reload();
+
+    document.getElementById("startpage").innerHTML = finalQuizDetails;
+}
+
+
+//wrong answer reduces time by 5 seconds
+function wrongAnswer() {
+    timeLeft -= 5;
+
+    if (timeLeft < 0) {
+        timeLeft = 0;
+    }
+    feedbackEl.textContent = "WRONG! The answer was: " + questions[nextQuestion].answer;
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function () {
+        feedbackEl.setAttribute("class", "hide");
+    }, 1500);
+
+    nextNewQuestion();
+
+}
+
+
+//correct answer grants the user 40 points/ maxmimum user high score is 400!
+function correctAnswer() {
+    totalScore += 40;
+
+    feedbackEl.textContent = "Awesome, you got it! " + questions[nextQuestion].answer;
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function () {
+        feedbackEl.setAttribute("class", "hide");
+    }, 1500);
+
+    nextNewQuestion();
+
+}
+
+//officially initates the question const and ensures correct answers are chosed, linking them to the above functions
+function nextNewQuestion() {
+    nextQuestion++;
+
+    if (nextQuestion > questions.length - 1) {
+        endQuiz();
+        return;
+    }
+
+    var finalQuizDetails = "<h2>" + questions[nextQuestion].question + "</h2>"
+
+    for (var i = 0; i < questions[nextQuestion].choices.length; i++) {
+        var choiceButton = "<button onclick = \"[answerchoice]\">[CHOICE]</button>";
+        choiceButton = choiceButton.replace("[CHOICE]", questions[nextQuestion].choices[i]);
+        if (questions[nextQuestion].choices[i] === questions[nextQuestion].answer) {
+            choiceButton = choiceButton.replace("[answerchoice]", "correctAnswer()");
+        } else {
+            choiceButton = choiceButton.replace("[answerchoice]", "wrongAnswer()");
+        }
+        finalQuizDetails += choiceButton
+    }
+    document.getElementById("startpage").innerHTML = finalQuizDetails;
+}
